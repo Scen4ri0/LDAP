5cf5fb73dec48c11bcc12931a30a7fbf0664fa8d
Scen4ri0 <mr.vadpop2004@mail.ru>
2023-01-26 10:59:41 +0300

    fixed bugs

--- src/LDAP.cc
+++ src/LDAP.cc
@@ -14,60 +14,59 @@ LDAP::LDAP(Connection* c)
 
 : tcp::TCP_ApplicationAnalyzer("LDAP", c)
 
- {
- interp = new binpac::LDAP::LDAP_Conn(this);
+ 	{
+ 	interp = new binpac::LDAP::LDAP_Conn(this);
  
- had_gap = false;
- 
- }
+	had_gap = false;
+ 	}
 
 LDAP::~LDAP()
- {
- delete interp;
- }
+ 	{
+ 	delete interp;
+ 	}
 
 void LDAP::Done()
- {
+ 	{
  
- tcp::TCP_ApplicationAnalyzer::Done();
+	tcp::TCP_ApplicationAnalyzer::Done();
 
- interp->FlowEOF(true);
- interp->FlowEOF(false);
+ 	interp->FlowEOF(true);
+ 	interp->FlowEOF(false);
  
- }
+ 	}
 
 void LDAP::EndpointEOF(bool is_orig)
- {
- tcp::TCP_ApplicationAnalyzer::EndpointEOF(is_orig);
- interp->FlowEOF(is_orig);
- }
+ 	{
+ 	tcp::TCP_ApplicationAnalyzer::EndpointEOF(is_orig);
+ 	interp->FlowEOF(is_orig);
+ 	}
 
 void LDAP::DeliverStream(int len, const u_char* data, bool orig)
- {
- LDAP::DeliverStream(len, data, orig);
+ 	{
+ 		LDAP::DeliverStream(len, data, orig);
 
- assert(TCP());
- if ( TCP()->IsPartial() )
-  return;
+ 		assert(TCP());
+ 		if ( TCP()->IsPartial() )
+  			return;
 
- if ( had_gap )
+ 		if ( had_gap )
   // If only one side had a content gap, we could still try to
   // deliver data to the other side if the script layer can handle this.
-  return;
+  		return;
 
- try
-  {
-  interp->NewData(orig, data, data + len);
-  }
- catch ( const binpac::Exception& e )
-  {
-  ProtocolViolation(zeek::util::fmt("Binpac exception: %s", e.c_msg()));
-  }
- }
+ 		try
+  			{
+  			interp->NewData(orig, data, data + len);
+  			}
+ 		catch ( const binpac::Exception& e )
+  			{
+  			ProtocolViolation(zeek::util::fmt("Binpac exception: %s", e.c_msg()));
+  			}
+ 	}
 
 void LDAP::Undelivered(uint64_t seq, int len, bool orig)
- {
- Analyzer::Undelivered(seq, len, orig);
- had_gap = true;
- interp->NewGap(orig, len);
- }
+ 	{
+ 		Analyzer::Undelivered(seq, len, orig);
+ 		had_gap = true;
+ 		interp->NewGap(orig, len);
+ 	}
--- src/LDAP.h
+++ src/LDAP.h
@@ -11,26 +11,26 @@ namespace zeek::analyzer::ldap {
 
 class LDAP: public tcp::TCP_ApplicationAnalyzer
 {
-public:
- LDAP(Connection* conn);
- virtual ~LDAP();
+	public:
+ 	 LDAP(Connection* conn);
+ 	 virtual ~LDAP();
 
  // Overriden from Analyzer.
- virtual void Done();
+ 	virtual void Done();
  
- virtual void DeliverStream(int len, const u_char* data, bool orig);
- virtual void Undelivered(uint64_t seq, int len, bool orig);
+ 	virtual void DeliverStream(int len, const u_char* data, bool orig);
+ 	virtual void Undelivered(uint64_t seq, int len, bool orig);
 
  // Overriden from tcp::TCP_ApplicationAnalyzer.
- virtual void EndpointEOF(bool is_orig);
+ 	virtual void EndpointEOF(bool is_orig);
  
 
- static analyzer::Analyzer* InstantiateAnalyzer(Connection* conn)
-  { return new LDAP(conn); }
+ 	static analyzer::Analyzer* InstantiateAnalyzer(Connection* conn)
+  	{ return new LDAP(conn); }
 
-protected:
- binpac::LDAP::LDAP_Conn* interp;
- bool had_gap;
- };
+	protected:
+ 	 binpac::LDAP::LDAP_Conn* interp;
+ 	 bool had_gap;
+};
 
 } // namespace analyzer::*
